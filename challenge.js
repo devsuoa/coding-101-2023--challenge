4a96898253cd18732175f8c5e9849b9e5935364a
KeiHara <keihara453@gmail.com>
2023-03-05 18:13:31 +1300

    adding challenge

---
+++ challenge.js
@@ -0,0 +1,14 @@
+function displayEvenNumbers(){
+    let numbers = [1,2,3,4,5,6,7,8];
+    let evenNumbers = [];
+    for(let i=0; i<numbers.length-1; i++;){
+        if(numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+displayEvenNumbers(); // should return [2,4,6,8]
+
+// HINT - eight things need to be changed here for this to work!
+// Start by fixing the syntax errors and then run the function to see what your output is.
